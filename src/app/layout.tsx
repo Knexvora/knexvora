8803306483e41f6cfff1c0117a72ca8cd7c16958
Saisudha06 <2203031240120@paruluniversity.ac.in>
2026-06-03 11:59:24 +0530

    upgrade: Next.js 16 + ESLint 9, fix CSS import order, migrate to flat ESLint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,22 +96,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,300..700;1,8..60,300..700&family=Hanken+Grotesk:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          rel="stylesheet"
-        />
         <JsonLd data={organizationSchema} />
       </head>
       <body className="bg-background text-on-background overflow-x-hidden">
